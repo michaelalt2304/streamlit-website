78094b448c559fed6c73ffb8b0d44b523c73371e
Michael Straus <mstraus@oxy.edu>
2024-07-07 17:00:53 -0400

    Added json for cloud storage inside of dep

--- Hello.py
+++ Hello.py
@@ -3,20 +3,18 @@ st.set_page_config(
     page_title="OddAI",
     page_icon="👋",
 )
-# st.session_state.PORT_NUMBER = 3311
 
 from dep import *
+
+
 st.write('# Sign in to access your files')
 new_user = st.checkbox("Create Account", value=False)
-# print(new_user)
 
 if 'user' not in st.session_state:
     st.session_state['user'] = 'Guest'
 
 user = st.text_input("Username", "")
 pwd = st.text_input("Password", "")
-# print(username)
-# print(st.session_state.Username + "Current username")
 st.session_state.user = "Guest"
 
 success = add_user(user, pwd, new_user)

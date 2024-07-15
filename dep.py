ca5029fbadd9dd6e043ef329b0efda99175cba0a
Michael Straus <mjstraus2304@gmail.com>
2024-07-15 09:12:58 -0400

    Adding what was published to GCloud

--- dep.py
+++ dep.py
@@ -26,7 +26,10 @@ from google.cloud.sql.connector import Connector, IPTypes
 import pymysql
 import sqlalchemy
 from sqlalchemy.sql import text
-from switch_page import *
+from streamlit.runtime.scriptrunner import RerunData, RerunException
+from streamlit.source_util import get_pages
+
+
 
 temp_folder = os.path.join('.', 'Files_local')
 temp_weights = os.path.join(temp_folder, 'Weights')
@@ -106,6 +109,36 @@ def run_sql(prompt: str, write = False):
             return pretty_dict
         else:
             con.commit()
+
+
+def switch_page(page_name: str):
+    """
+    Switch page programmatically in a multipage app
+
+    Args:
+        page_name (str): Target page name
+    """
+
+
+    def standardize_name(name: str) -> str:
+        return name.lower().replace("_", " ")
+
+    page_name = standardize_name(page_name)
+
+    pages = get_pages("streamlit_app.py")  # OR whatever your main page is called
+
+    for page_hash, config in pages.items():
+        if standardize_name(config["page_name"]) == page_name:
+            raise RerunException(
+                RerunData(
+                    page_script_hash=page_hash,
+                    page_name=page_name,
+                )
+            )
+
+    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
+
+    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
 
 ###########################
 # GOOGLE HELPER FUNCTIONS #
